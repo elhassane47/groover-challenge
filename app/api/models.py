abb81ef8d6765df2af3c99afea94eb920b21dc85
elhassane <elhassanegargem@gmail.com>
2020-11-05 21:53:45 +0100

    model artist

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=150)
+    href = models.URLField()
+    spotify_id = models.CharField(max_length=150)
+    type = models.CharField(max_length=100)
+    uri = models.CharField(max_length=255)
+    external_urls = models.JSONField()
+
+    def __str__(self):
+        return self.name
