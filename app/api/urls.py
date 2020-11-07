7006bddbfb8d41e7b957ba6faad079e899998246
elhassane <elhassanegargem@gmail.com>
2020-11-07 15:46:02 +0100

    artist viewset & login & callback api views

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from api.views import ArtistViewSet
+from api.views import ArtistViewSet, callback, login_spotify
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -9,5 +9,7 @@ router.register('artists', ArtistViewSet, basename='artists')
 app_name = 'api'
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('auth/callback', callback,name='spotify-callback'),
+    path('login_spotify', login_spotify, name='spotify-login')
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,8 +1,94 @@
+from django.db import IntegrityError
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework import status
+from django.urls import reverse
+from requests.exceptions import HTTPError
+
 from api.serializers import ArtistSerializer
 from api.models import Artist
+from api.spotify_api import SpotifyApi
+from api.spotify_auth import SpotifyAuth
+from django.http.response import  HttpResponseRedirect
+import pdb
 
 
 class ArtistViewSet(ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
+    auth_api = SpotifyAuth()
+
+    @action(detail=False, methods=['GET'])
+    def new_realses(self, request):
+
+        tokens = request.session.get('tokens')
+        # if the token expired in session =>redirect to login
+        if not tokens:
+            return HttpResponseRedirect(redirect_to=reverse('api:spotify-login'))
+
+        sp = SpotifyApi(token=tokens.get('access_token'))
+
+        try:
+            data = sp.artists_new_relases()
+
+        except HTTPError:
+            return Response(status=status.HTTP_401_UNAUTHORIZED,data={"message": "invalid spotify token"})
+
+            # refresh_token = tokens.get('refresh_token')
+            # token = self.auth_api.refreshAuth(refresh_token)
+            # sp.set_token(token)
+            # data = sp.artists_new_relases()
+            # HttpResponseRedirect(redirect_to=reverse('api:spotify-login'))
+            # pdb.set_trace()
+            response = []
+            errors = []
+            for artist in data:
+                serializer = ArtistSerializer(data=artist)
+                if serializer.is_valid():
+                    try:
+                        serializer.save()
+                    #     already existed artists with this spotify_id
+                    except IntegrityError as e:
+                        errors.append(e)
+
+                    else:
+                        response.append(serializer.data)
+                else:
+                    errors.append(serializer.errors)
+
+            return Response(status=200, data={"release": response})
+
+
+def callback(request):
+
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+    auth_api = SpotifyAuth()
+
+    if error:
+        payload = {
+            'grant_type': 'authorization_code',
+            'error': error,
+
+        }
+        return JsonResponse(status=status.HTTP_401_UNAUTHORIZED, data=payload)
+    else:
+
+        resp = auth_api.getUserToken(code)
+        request.session['tokens'] = resp
+
+        return JsonResponse(
+            status=status.HTTP_200_OK,
+            data={'message': "you are successfuly logged in to Spotify api",
+                  'tokens': resp,
+                  "new releases endpoint": reverse(('api:artists-new-realses'))})
+
+
+def login_spotify(request):
+
+    auth_api = SpotifyAuth()
+    code_url = auth_api.getUser()
+
+    return HttpResponseRedirect(redirect_to=code_url)
