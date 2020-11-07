af13b5477181d55b39f76a25c8085915d35b5c12
elhassane <elhassanegargem@gmail.com>
2020-11-07 15:45:02 +0100

    artist serializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -4,8 +4,12 @@ from api.models import Artist
 
 class ArtistSerializer(serializers.ModelSerializer):
 
+    #the id field in spotify api is stored as spotify_id in our model
+
+    id = serializers.CharField(source='spotify_id')
+
     class Meta:
         model = Artist
-        fields = ('pk', 'name', 'href', 'spotify_id', 'type', 'uri', 'external_urls',)
+        fields = ('pk', 'name', 'href', 'id', 'type', 'uri', 'external_urls',)
 
 
